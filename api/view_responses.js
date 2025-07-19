039535e2640ffe20a68cbc6d4ff2786b279a28d9
sachinthalore52 <sachinthalore52@gmail.com>
2025-07-19 23:57:47 +0530

    Update view_responses.js

--- api/view_responses.js
+++ api/view_responses.js
@@ -1,21 +1,18 @@
-import mysql from 'mysql2/promise';
+import { createClient } from '@supabase/supabase-js';
+
+const SUPABASE_URL = 'https://comdfcrvqifcbtntdofs.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNvbWRmY3J2cWlmY2J0bnRkb2ZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI5NDc1MzAsImV4cCI6MjA2ODUyMzUzMH0.usg1Sp-yMteILv5qwgEd6hsvrZ10TVe42_9-z7tKJaU';
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export default async function handler(req, res) {
-  try {
-    const connection = await mysql.createConnection({
-      host: 'centerbeam.proxy.rlwy.net',
-      port: 22353,
-      user: 'root',
-      password: 'BrEYLiPHKiaXpChPWGzsYqvwpaRgFKUq',
-      database: 'railway'
-    });
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
-    const [rows] = await connection.execute('SELECT * FROM responses ORDER BY created_at DESC');
-    await connection.end();
+  const { data, error } = await supabase
+    .from('responses')
+    .select('*')
+    .order('timestamp', { ascending: false });
 
-    res.status(200).json(rows);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error fetching data');
-  }
+  if (error) return res.status(500).send("Error fetching data");
+
+  res.status(200).json(data);
 }
